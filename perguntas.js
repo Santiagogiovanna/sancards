d42183e96ef89ca71efafd1d72ebf5463133c200
Santiagogiovanna <00001108867972sp@al.educacao.sp.gov.br>
2024-11-28 21:06:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Cinema',
+    'Com quantos anos se entra em Hogwarts?',
+    'Com 11 anos'
+)
+
+criaCartao(
+    'Cinema',
+    'Quais as quatro casas de Hogwarts?',
+    'Grifinória,Lufa lufa,Corvinal e Sonserina'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual a casa de Harry Potter?',
+    'Grifinória'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual a casa do vilão Voldemort?',
+    'Sonserina'
+)
+criaCartao(
+    'Cinema',
+    'Qual a casa que Harry não queria fazer parte?',
+    'Sonserina'
+)
